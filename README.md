5603a899c44397b6357637e34eb807c49529d6f9
Jubayer Hasan <116904777+Jubayerasan@users.noreply.github.com>
2023-04-30 01:34:28 +0800

    Update HTML, CSS, JS File

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Project Assignment
 
 robbani ghozi fikri 1832765
+
+alfikra muhammad 1836031
+
+amiruddin 2013325
+
 MD JUBAYER HASAN 1837373
---
+++ pos_system/bin/pos_system.dart
@@ -0,0 +1,5 @@
+import 'package:pos_system/pos_system.dart' as pos_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pos_system.calculate()}!');
+}
