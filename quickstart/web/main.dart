b8b3c953f37b60137c69819f8ce516733fbdd5b1
ghozifr <ghozifr@gmail.com>
2023-05-01 13:58:25 +0800

    cart

--- quickstart/web/main.dart
+++ quickstart/web/main.dart
@@ -1,14 +1,46 @@
 import 'dart:html';
 
 void main() {
-  // Get a reference to all the "Add to Cart" buttons
-  List<ButtonElement> buttons = querySelectorAll('button');
+  List<Map<String, dynamic>> cartItems = []; // holds the list of items in cart
 
-  // Add an event listener to each button
-  buttons.forEach((button) {
-    button.onClick.listen((MouseEvent event) {
-      // Add your code here to handle the button click
-      print('Button clicked!');
+  // Get the add-to-cart buttons and add click event listeners to them
+  List<Element> addToCartBtns = querySelectorAll('.add-to-cart');
+  addToCartBtns.forEach((btn) {
+    btn.onClick.listen((event) {
+      // Get the data-name and data-price attributes from the button element
+      String itemName = btn.dataset['name'];
+      double itemPrice = double.parse(btn.dataset['price']);
+
+      // Add the item to the cartItems list
+      cartItems.add({
+        'name': itemName,
+        'price': itemPrice,
+      });
+
+      // Update the cart display
+      updateCartDisplay(cartItems);
     });
   });
+}
+
+// Update the cart display with the current list of cart items
+void updateCartDisplay(List<Map<String, dynamic>> cartItems) {
+  // Get the cart items list and total price span elements
+  Element cartItemsList = querySelector('#cart-items');
+  Element totalPriceSpan = querySelector('#total-price');
+
+  // Clear the current contents of the cart items list
+  cartItemsList.innerHtml = '';
+
+  // Add each cart item to the cart items list
+  double totalPrice = 0;
+  cartItems.forEach((item) {
+    Element li = LIElement()
+      ..text = '${item['name']} - \$${item['price'].toStringAsFixed(2)}';
+    cartItemsList.children.add(li);
+    totalPrice += item['price'];
+  });
+
+  // Update the total price span with the new total price
+  totalPriceSpan.text = totalPrice.toStringAsFixed(2);
 }
